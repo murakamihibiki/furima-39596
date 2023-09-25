89b05776d881fd1d893dae1a774c25762e5c6363
murakamihibiki <rstoyo.hibiki.murakami@gmail.com>
2023-09-25 14:54:50 +0900

    ユーザー管理変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         validates :nickname, presence: true
-         validates :email, presence: true
-         validates :encrypted_password, presence: true
-         validates :last_name, presence: true
-         validates :first_name, presence: true
-         validates :last_name_kana, presence: true
-         validates :first_name_kana, presence: true
-         validates :birth_date, presence: true
+
+  validates :nickname,  presence: true
+  validates :birth_date, presence: { message: "date can't be blank" }
+
+  PASSWORD_REGEX = /\A(?=.*?[a-zA-Z])(?=.*?\d)[A-Za-z0-9]+\z/.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers' 
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters'} do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters'} do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
+  has_many :items
+  has_many :purchase_records
 end
