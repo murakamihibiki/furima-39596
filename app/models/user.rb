b03a753c0ef356ac7c91f3bd683c9a4373bac8d9
murakamihibiki <rstoyo.hibiki.murakami@gmail.com>
2023-09-27 14:32:21 +0900

    修正依頼修正完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,  presence: true
-  validates :birth_date, presence: { message: "date can't be blank" }
+  validates :nick_name,  presence: true
+  validates :birthday, presence: { message: "date can't be blank" }
 
   PASSWORD_REGEX = /\A(?=.*?[a-zA-Z])(?=.*?\d)[A-Za-z0-9]+\z/.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers' 
@@ -23,3 +23,4 @@ class User < ApplicationRecord
   has_many :items
   has_many :purchase_records
 end
+
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname {'ニックネーム'}
-    first_name {'名'}
-    last_name {'姓'}
-    first_name_kana {'メイ'}
-    last_name_kana {'セイ'}
-    birth_date {'1930-02-04'}
-    email {'test@example'}
-    password {'pass123'}
-        
+    nick_name              {'ニックネーム'}
+    first_name             {'名'}
+    last_name              {'姓'}
+    first_name_kana        {'メイ'}
+    last_name_kana         {'セイ'}
+    birthday               {'1930-02-04'}
+    email                  {Faker::Internet.email}
+    password               {'pass123'}
+    password_confirmation  {'pass123'}
   end
 end
