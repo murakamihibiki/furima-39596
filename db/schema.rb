3ee1a93cdf5b8d61466b73dac96d0287a739787c
murakamihibiki <rstoyo.hibiki.murakami@gmail.com>
2023-10-04 18:36:14 +0900

    １２

--- db/schema.rb
+++ db/schema.rb
@@ -55,8 +55,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_02_082333) do
   end
 
   create_table "items", charset: "utf8", force: :cascade do |t|
+    t.string "item_name", null: false
+    t.text "description", null: false
+    t.integer "category_id", null: false
+    t.integer "condition_id", null: false
+    t.integer "shipping_charge_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "delivery_date_id", null: false
+    t.integer "price", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "prefectures", charset: "utf8", force: :cascade do |t|
@@ -89,4 +99,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_02_082333) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "items", "users"
 end
