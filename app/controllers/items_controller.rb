324533e18d40100402bf98155d7b9a3980484765
murakamihibiki <rstoyo.hibiki.murakami@gmail.com>
2023-10-06 15:11:01 +0900

    99

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,12 +2,12 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :destroy, :edit]
   before_action :set_item, only: [:show, :edit, :update, :destroy] 
 
-  def index
-    @items = Item.order("created_at DESC")
-  end
+  #def index
+    #@items = Item.order("created_at DESC")
+  #end
 
   def show
-    @purchase_records = PurchaseRecord.where(item_id: @item.id)
+    #@purchase_records = PurchaseRecord.where(item_id: @item.id)
   end
 
   def new
@@ -29,29 +29,29 @@ class ItemsController < ApplicationController
   end
   
   def edit
-    @purchase_records = PurchaseRecord.where(item_id: @item.id)
-    if current_user != @item.user || (@purchase_records.present? && @purchase_records.pluck(:item_id).include?(@item.id))
-      redirect_to action: :index
-    else
+    #@purchase_records = PurchaseRecord.where(item_id: @item.id)
+    #if current_user != @item.user || (@purchase_records.present? && @purchase_records.pluck(:item_id).include?(@item.id))
+    #redirect_to action: :index
+    #else
       render :edit
     end
   end
 
   def update
-    if @item.update(item_params)
-      redirect_to item_path
-    else
-      render :edit, status: :unprocessable_entity
+    #if @item.update(item_params)
+      #redirect_to item_path
+    #else
+      #render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
-    if current_user == @item.user
-      @item.destroy
-    else
-      redirect_to item_path
-    end
-    redirect_to action: :index
+    #if current_user == @item.user
+      #@item.destroy
+    #else
+      #redirect_to item_path
+    #end
+    #redirect_to action: :index
   end
 
   private
