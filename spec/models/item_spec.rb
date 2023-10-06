ea02d91e0fd0119bd582d4e2f24b93e594df3aed
murakamihibiki <rstoyo.hibiki.murakami@gmail.com>
2023-10-06 15:30:27 +0900

    00

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -79,8 +79,9 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price is out of setting range")
       end
       it 'ユーザーが紐付いていないと保存できない' do
-        item = FactoryBot.build(:item, user: nil)
-        expect(item).to_not be_valid
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
       end
     end
     
