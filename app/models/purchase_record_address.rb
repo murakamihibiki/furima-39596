c63eab813c124ced9b480ace53854710a13d5003
murakamihibiki <rstoyo.hibiki.murakami@gmail.com>
2023-10-11 17:20:22 +0900

    商品購入機能の大元

---
+++ app/models/purchase_record_address.rb
@@ -0,0 +1,23 @@
+class PurchaseRecordAddress
+  include ActiveModel::Model
+
+  attr_accessor :post_code, :prefecture_id, :municipality, :street_address, :building_name, :telephone_number, :user_id,
+                :item_id, :token
+
+  with_options presence: true do
+    validates :post_code, format: { with: /\A\d{3}-\d{4}\z/ }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :municipality
+    validates :street_address
+    validates :telephone_number, length: { in: 10..11 }, numericality: { only_integer: true }
+    validates :item_id
+    validates :user_id
+    validates :token
+  end
+
+  def save
+    purchase_record = PurchaseRecord.create(user_id:, item_id:)
+    Address.create(post_code:, prefecture_id:, municipality:,
+                   street_address:, building_name:, telephone_number:, purchase_record_id: purchase_record.id)
+  end
+end
